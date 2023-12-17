49b6dd23db3a60dcb82cfdc9a803287cab5a2224
Matheus Braum <matheus_braum1@hotmail.com>
2023-12-17 00:21:12 -0300

    Deploy

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,7 +1,13 @@
 export const environment = {
   production: false,
-  apiAccountUrl: 'https://localhost:7282/account',
-  apiAcessoUrl: 'https://localhost:7282/acesso',
-  apiCapitaoUrl: 'https://localhost:7282/capitao',
-  apiPirataUrl: 'https://localhost:7282/pirata',
+  apiURL: 'https://binks-no-sake.onrender.com',
+  apiAccountUrl: 'https://binks-no-sake.onrender.com/Account',
+  apiAcessoUrl: 'https://binks-no-sake.onrender.com/Acesso',
+  apiCapitaoUrl: 'https://binks-no-sake.onrender.com/Capitao',
+  apiPirataUrl: 'https://binks-no-sake.onrender.com/Pirata',
+
+  // apiAccountUrl: 'https://localhost:7282/account',
+  // apiAcessoUrl: 'https://localhost:7282/acesso',
+  // apiCapitaoUrl: 'https://localhost:7282/capitao',
+  // apiPirataUrl: 'https://localhost:7282/pirata',
 }
